84e30d7dfb0e7e2e9eee94b1d3f2bb7e6305f857
unknown <42Shermann>
2022-01-04 16:08:18 +0700

    ESLINT fixed

--- src/components/ConstrDetail/ConstrDetail.js
+++ src/components/ConstrDetail/ConstrDetail.js
@@ -1,48 +1,47 @@
-import React from 'react';
+import React from 'react'
 import Tab from 'react-bootstrap/Tab'
 import Tabs from 'react-bootstrap/Tabs'
 import Image from 'react-bootstrap/Image'
-import { getRank, getType, getFaction, getGift, getElement } from '../../constants/DATA';
-import TabComp from '../Tab/Tab';
-import Stats from '../Stats/Stats';
-import './ConstrDetail.css';
+import { getRank, getType, getFaction, getGift, getElement } from '../../constants/DATA'
+import TabComp from '../Tab/Tab'
+import Stats from '../Stats/Stats'
+import './ConstrDetail.css'
 
-function ConstrDetail({data}) {
+function ConstrDetail ({ data }) {
+  const tag = {
+    rank: getRank(data.rank),
+    type: getType(data.type),
+    faction: getFaction(data.faction),
+    gift: getGift(data.gift)
+  }
 
-    const tag = {
-        rank:getRank(data.rank),
-        type:getType(data.type),
-        faction:getFaction(data.faction),
-        gift:getGift(data.gift),
-      } 
-      
-    const ElementComponent = ({values}) =>{
-      const { img } = getElement(values.eleType);
-      return(
-        <div className="col">
-          <img className="img-fluid mx-auto d-block" src={img} alt={values.eleType}/>
-          <p>{values.eleType} ({values.icon})</p>
-      </div>
-      )
-    }  
-    
-    //Render element type
-    const RenderElement = 
-        data.element.map((value, index) =>
-          (
-            <ElementComponent key={index} values={value} />
-          )
-        )
-
+  const ElementComponent = ({ values }) => {
+    const { img } = getElement(values.eleType)
     return (
-        <div className="container-fluid bg-color">
-        <div>
-          <div className="h1 text-white text-center text-bold">{data.name} : {data.Model}</div>
-        </div>
-        <div>
+      <div className="col">
+        <img className="img-fluid mx-auto d-block" src={img} alt={values.eleType}/>
+        <p>{values.eleType} ({values.icon})</p>
+      </div>
+    )
+  }
+
+  // Render element type
+  const RenderElement =
+    data.element.map((value, index) =>
+      (
+        <ElementComponent key={index} values={value} />
+      )
+    )
+
+  return (
+    <div className="container-fluid bg-color">
+      <div>
+        <div className="h1 text-white text-center text-bold">{data.name} : {data.Model}</div>
+      </div>
+      <div>
         <Tabs defaultActiveKey="overview" id="uncontrolled-tab-example" className="main-tab" >
           <Tab eventKey="overview" title="Overview">
-          <div className="row">
+            <div className="row">
               <div className="bg-white my-3 ">
                 <figure className="text-start ">
                 <blockquote className="blockquote my-4 lead text-black">
@@ -51,21 +50,21 @@ function ConstrDetail({data}) {
               </figure>
               </div>
             </div>
-            <div className="row">
-              <div className="col-md-4 col-xm-12">
-                <img className="img-fluid" src={data.img} alt={data.Model} />
-                <div className="spec-bg text-white">
+          <div className="row">
+            <div className="col-md-4 col-xm-12">
+              <img className="img-fluid" src={data.img} alt={data.Model} />
+              <div className="spec-bg text-white">
                 <div className="py-4">
                   <div className="icon-container">
-                      <div>
-                        <img className="img-type" src={data.chibiImg.awaken} alt="awaken"/>
-                      </div>
-                      <div>
-                        <img className="img-type" src={data.chibiImg.avatar} alt="avatar"/>
-                      </div>
-                      <div>
-                        <img className="img-type" src={data.chibiImg.dorm} alt="dorm"/>
-                      </div>
+                    <div>
+                      <img className="img-type" src={data.chibiImg.awaken} alt="awaken"/>
+                    </div>
+                    <div>
+                      <img className="img-type" src={data.chibiImg.avatar} alt="avatar"/>
+                    </div>
+                    <div>
+                      <img className="img-type" src={data.chibiImg.dorm} alt="dorm"/>
+                    </div>
                   </div>
                 </div>
                 <div className="">
@@ -133,7 +132,7 @@ function ConstrDetail({data}) {
           <Tab eventKey="gallery" title="Gallery">
             <Tabs defaultActiveKey={1} id="coating-tab" className='mt-2'>
             {
-              data.coating.map(item=>(
+              data.coating.map(item => (
                 <Tab key={item.id} eventKey={item.id} title={item.name}>
                   <div className='d-flex justify-content-center coating-container'>
                     <Image className='mx-auto d-block' fluid={true} src={item.img} alt={item.name} />
@@ -153,7 +152,7 @@ function ConstrDetail({data}) {
             <div className="mb-4 border border-top border-white" />
           </div>
       </div>
-    );
+  )
 }
 
-export default ConstrDetail;
+export default ConstrDetail
--- src/components/ConstrDetail/detailPlaceholder.js
+++ src/components/ConstrDetail/detailPlaceholder.js
@@ -1,206 +1,204 @@
-import React from 'react';
+import React from 'react'
 import Placeholder from 'react-bootstrap/Placeholder'
 import Tab from 'react-bootstrap/Tab'
 import Tabs from 'react-bootstrap/Tabs'
 
-
-function DetailPlaceholder() {
-
-    const PHStats = () =>{
-      return(
+function DetailPlaceholder () {
+  const PHStats = () => {
+    return (
+    <div>
+      <div className="container-fluid text-white stats-bg">
         <div>
-            <div className="container-fluid text-white stats-bg">
-            <div>
-              <Placeholder as="h2" animation="glow">
-                  <Placeholder xs={4}/>
+          <Placeholder as="h2" animation="glow">
+            <Placeholder xs={4}/>
+          </Placeholder>
+        </div>
+        <div className="row">
+          <div className="mb-4 border border-top border-white" />
+        </div>
+        <div className="row my-4">
+          <div className="col-6">
+            <div className="row">
+              {/* Service Time */}
+              <Placeholder as="p" animation="glow">
+                <Placeholder xs={4}/>
+              </Placeholder>
+              <Placeholder as="p" animation="glow">
+                <Placeholder xs={2}/>
               </Placeholder>
             </div>
             <div className="row">
-                <div className="mb-4 border border-top border-white" />
-            </div>
-            <div className="row my-4">
-                <div className="col-6">
-                    <div className="row">
-                    {/*Service Time */}
-                      <Placeholder as="p" animation="glow">
-                          <Placeholder xs={4}/>
-                      </Placeholder>
-                        <Placeholder as="p" animation="glow">
-                          <Placeholder xs={2}/>
-                      </Placeholder>
-                    </div>
-                    <div className="row">
-                    {/*Psychological Age */}
-                    <Placeholder as="p" animation="glow">
-                          <Placeholder xs={5}/>
-                      </Placeholder>
-                        <Placeholder as="p" animation="glow">
-                          <Placeholder xs={2}/>
-                      </Placeholder>
-                    </div>
-                    <div className="row">
-                      {/*Activation Date*/}
-                      <Placeholder as="p" animation="glow">
-                          <Placeholder xs={6}/>
-                      </Placeholder>
-                        <Placeholder as="p" animation="glow">
-                          <Placeholder xs={5}/>
-                      </Placeholder>
-                    </div>    
-                    <div className="row">
-                      {/*Height*/}
-                      <Placeholder as="p" animation="glow">
-                          <Placeholder xs={3}/>
-                      </Placeholder>
-                        <Placeholder as="p" animation="glow">
-                          <Placeholder xs={3}/>
-                      </Placeholder>
-                    </div>
-                    <div className="row">
-                      {/*Weight*/}
-                      <Placeholder as="p" animation="glow">
-                          <Placeholder xs={3}/>
-                      </Placeholder>
-                        <Placeholder as="p" animation="glow">
-                          <Placeholder xs={2}/>
-                      </Placeholder>
-                    </div>    
-                    <div className="row">
-                      {/*Vital Fluid Type */}
-                      <Placeholder as="p" animation="glow">
-                          <Placeholder xs={5}/>
-                      </Placeholder>
-                        <Placeholder as="p" animation="glow">
-                          <Placeholder xs={2}/>
-                      </Placeholder>
-                    </div>
-              </div>
-                <div className="col-12 col-md-6  divider">
-                  <div className='row'>
-                    {/*Fake Chart */}
-                    <div className='loading-image mx-2' style={{height:'300px'}} />
-                  </div>
-                    <div className="row text-center">
-                        <div className="col ">
-                          {/*HP */}
-                          <Placeholder as="p" animation="glow">
-                              <Placeholder xs={5}/>
-                          </Placeholder>
-                            <Placeholder as="p" animation="glow">
-                              <Placeholder xs={5}/>
-                          </Placeholder>
-                        </div>
-                        <div className="col ">
-                            {/*DEF */}
-                            <Placeholder as="p" animation="glow">
-                              <Placeholder xs={5}/>
-                          </Placeholder>
-                            <Placeholder as="p" animation="glow">
-                              <Placeholder xs={5}/>
-                          </Placeholder>
-                        </div>
-                        <div className="col">
-                            {/*ATK */}
-                            <Placeholder as="p" animation="glow">
-                              <Placeholder xs={5}/>
-                          </Placeholder>
-                            <Placeholder as="p" animation="glow">
-                              <Placeholder xs={5}/>
-                          </Placeholder>
-                        </div>
-                        <div className="col">
-                            {/*CRIT */}
-                            <Placeholder as="p" animation="glow">
-                              <Placeholder xs={5}/>
-                          </Placeholder>
-                            <Placeholder as="p" animation="glow">
-                              <Placeholder xs={5}/>
-                          </Placeholder>
-                        </div>  
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>
-      )
-    }
-
-    return (
-        <div className="container-fluid bg-color">
-        <div className="h1 text-white text-center text-bold">
-        <Placeholder as="p" animation="glow">
-            <Placeholder xs={6} />
-        </Placeholder>
-        </div>
-        <div>
-        <Tabs defaultActiveKey="overview" id="uncontrolled-tab-example" className="main-tab" >
-          <Tab eventKey="overview" title="Overview">
-          <div className="row">
-              <div className="bg-white my-3 ">
-                <figure className="text-start ">
-                <Placeholder as={'blockquote'} className="blockquote my-4 lead text-black">
-                  <Placeholder as="p" animation="glow">
-                        <Placeholder xs={12} bg="primary"/>
-                    </Placeholder>
+              {/* Psychological Age */}
+              <Placeholder as="p" animation="glow">
+                  <Placeholder xs={5}/>
                 </Placeholder>
-              </figure>
-              </div>
+                <Placeholder as="p" animation="glow">
+                  <Placeholder xs={2}/>
+                </Placeholder>
             </div>
-            <div className="row">
-              <div className="col-md-4 col-xm-12">
-                {/*Fake Image */}
-                <div className='loading-image' style={{height:'300px'}} />
-                <div className="spec-bg text-white">
-                <div className="py-4">
-                  <div className="icon-container" >
-                    <div className='loading-image' style={{height:'90px', width:'90px', backgroundColor:'black'}}/>
-                    <div className='loading-image' style={{height:'90px', width:'90px', backgroundColor:'black'}}/>
-                    <div className='loading-image' style={{height:'90px', width:'90px', backgroundColor:'black'}}/>
-                  </div>
-                </div>
-                <div className="">
-                  <div className="bg-danger text-center">
-                    <h4>Speciality</h4>
-                  </div>
-                  <div className="px-2">
-                  <Placeholder as="p" animation="glow">
-                      <Placeholder xs={12}/>
-                  </Placeholder>
-                  </div>
-                </div>
-                <div className="">
-                  <div className="bg-danger text-center">
-                    <h4>Element</h4>
-                  </div>
-                  <div className="row text-center">
-                  <Placeholder as="p" animation="glow">
-                      <Placeholder xs={5}/>
-                  </Placeholder>
-                  </div>
-                </div>
-                </div>
-              </div>
-              <div className="col-md-8 col-xm-12">
-                <PHStats />
-              </div>
-              <div className="col-md-8 col-xm-12">
-                <Placeholder as={'p'} />
-              </div>
-            </div>
-          </Tab>
-          <Tab eventKey="gallery" title="Gallery">
-            ....
-          </Tab>
-          <Tab eventKey="contact" title="Info" >
-            ....
-          </Tab>
-        </Tabs>
-      </div>
           <div className="row">
-            <div className="mb-4 border border-top border-white" />
+            {/* Activation Date */}
+            <Placeholder as="p" animation="glow">
+              <Placeholder xs={6}/>
+            </Placeholder>
+            <Placeholder as="p" animation="glow">
+              <Placeholder xs={5}/>
+            </Placeholder>
           </div>
+          <div className="row">
+            {/* Height */}
+            <Placeholder as="p" animation="glow">
+              <Placeholder xs={3}/>
+            </Placeholder>
+            <Placeholder as="p" animation="glow">
+              <Placeholder xs={3}/>
+            </Placeholder>
+          </div>
+          <div className="row">
+            {/* Weight */}
+            <Placeholder as="p" animation="glow">
+              <Placeholder xs={3}/>
+            </Placeholder>
+            <Placeholder as="p" animation="glow">
+              <Placeholder xs={2}/>
+            </Placeholder>
+          </div>
+          <div className="row">
+            {/* Vital Fluid Type */}
+            <Placeholder as="p" animation="glow">
+              <Placeholder xs={5}/>
+            </Placeholder>
+            <Placeholder as="p" animation="glow">
+              <Placeholder xs={2}/>
+            </Placeholder>
+          </div>
+        </div>
+        <div className="col-12 col-md-6  divider">
+          <div className='row'>
+          {/* Fake Chart */}
+          <div className='loading-image mx-2' style={{ height: '300px' }} />
+          </div>
+          <div className="row text-center">
+            <div className="col ">
+              {/* HP */}
+              <Placeholder as="p" animation="glow">
+                <Placeholder xs={5}/>
+              </Placeholder>
+              <Placeholder as="p" animation="glow">
+                <Placeholder xs={5}/>
+              </Placeholder>
+            </div>
+            <div className="col ">
+              {/* DEF */}
+              <Placeholder as="p" animation="glow">
+                <Placeholder xs={5}/>
+              </Placeholder>
+              <Placeholder as="p" animation="glow">
+                <Placeholder xs={5}/>
+              </Placeholder>
+            </div>
+            <div className="col">
+              {/* ATK */}
+              <Placeholder as="p" animation="glow">
+                <Placeholder xs={5}/>
+              </Placeholder>
+              <Placeholder as="p" animation="glow">
+                <Placeholder xs={5}/>
+              </Placeholder>
+            </div>
+            <div className="col">
+              {/* CRIT */}
+              <Placeholder as="p" animation="glow">
+                <Placeholder xs={5}/>
+              </Placeholder>
+              <Placeholder as="p" animation="glow">
+                <Placeholder xs={5}/>
+              </Placeholder>
+            </div>
+          </div>
+        </div>
       </div>
-    );
+    </div>
+    </div>
+    )
+  }
+
+  return (
+    <div className="container-fluid bg-color">
+    <div className="h1 text-white text-center text-bold">
+    <Placeholder as="p" animation="glow">
+      <Placeholder xs={6} />
+    </Placeholder>
+    </div>
+    <div>
+    <Tabs defaultActiveKey="overview" id="uncontrolled-tab-example" className="main-tab" >
+      <Tab eventKey="overview" title="Overview">
+      <div className="row">
+        <div className="bg-white my-3 ">
+        <figure className="text-start ">
+        <Placeholder as={'blockquote'} className="blockquote my-4 lead text-black">
+          <Placeholder as="p" animation="glow">
+            <Placeholder xs={12} bg="primary"/>
+          </Placeholder>
+        </Placeholder>
+        </figure>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-4 col-xm-12">
+        {/* Fake Image */}
+        <div className='loading-image' style={{ height: '300px' }} />
+        <div className="spec-bg text-white">
+        <div className="py-4">
+          <div className="icon-container" >
+          <div className='loading-image' style={{ height: '90px', width: '90px', backgroundColor: 'black' }}/>
+          <div className='loading-image' style={{ height: '90px', width: '90px', backgroundColor: 'black' }}/>
+          <div className='loading-image' style={{ height: '90px', width: '90px', backgroundColor: 'black' }}/>
+          </div>
+        </div>
+        <div className="">
+          <div className="bg-danger text-center">
+          <h4>Speciality</h4>
+          </div>
+          <div className="px-2">
+          <Placeholder as="p" animation="glow">
+            <Placeholder xs={12}/>
+          </Placeholder>
+          </div>
+        </div>
+        <div className="">
+          <div className="bg-danger text-center">
+          <h4>Element</h4>
+          </div>
+          <div className="row text-center">
+          <Placeholder as="p" animation="glow">
+            <Placeholder xs={5}/>
+          </Placeholder>
+          </div>
+        </div>
+        </div>
+        </div>
+        <div className="col-md-8 col-xm-12">
+        <PHStats />
+        </div>
+        <div className="col-md-8 col-xm-12">
+        <Placeholder as={'p'} />
+        </div>
+      </div>
+      </Tab>
+      <Tab eventKey="gallery" title="Gallery">
+      ....
+      </Tab>
+      <Tab eventKey="contact" title="Info" >
+      ....
+      </Tab>
+    </Tabs>
+    </div>
+      <div className="row">
+      <div className="mb-4 border border-top border-white" />
+      </div>
+    </div>
+  )
 }
 
-export default DetailPlaceholder;
+export default DetailPlaceholder
